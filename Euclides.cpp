709c87903218ea43dc4241ea55cde749a6688bf0
dpuertasg <dpuertasg@unicartagena.edu.co>
2026-01-05 15:21:49 -0500

    Add files via upload

---
+++ Euclides.cpp
@@ -0,0 +1,53 @@
+/*Elaborado por: 
+Dylan Maykol Puertas Girón 
+6902510052
+*/
+
+#include <iostream>
+#include <cmath>
+#include <string>
+#include <stdlib.h>
+
+using namespace std;
+
+int main(){
+int n1, n2, z, y;
+
+cout<<"ingrese primer numeros \n";
+cin>>n1;
+cout<<"ingrese segundo numero \n";
+cin>>n2;
+
+if (n1 > n2){
+	z = n1 % n2;
+	if(z == 0){
+		cout<<n2<<" es el maximo comun divisor";
+	}else{
+		while(z != 0){
+			y = z;
+			z = n2 % z;
+			
+	
+		}	cout<<y<<" es el maximo comun divisor";
+	}
+}else{
+if(n2 > n1){
+z = n2 % n1;
+if(z == 0){
+cout<<n1<<" es el maximo comun divisor";
+}else{
+	while(z != 0){
+	y = z;
+	z = n1 % z;
+	}cout<<y<<" es el maximo comun divisor";
+	}
+}else{
+	cout<<n1<<" es el maximo comun divisor";
+}
+}
+
+	
+	return 0;
+}
+ 
+
